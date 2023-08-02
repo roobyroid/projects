056cbbd5bd33d6a470229684994bfa2ad335150e
Oleg <mr.rooby@mail.ru>
2023-08-02 19:21:24 +0300

    Add text for faq

--- empire/source/js/modules/lang.js
+++ empire/source/js/modules/lang.js
@@ -1,6 +1,6 @@
 const lang = () => {
   const allLang = ['en', 'ru'];
-  let currentLang = 'en';
+  let currentLang = localStorage.getItem('language') || 'en';
 
   const textHomePage = {
     feedbackSoc: {
@@ -107,46 +107,98 @@ const lang = () => {
       en: 'What does your affiliate programm offer?',
       ru: 'Какие офферы предлагает ваша аффилиэйт программа?',
     },
+    answer1: {
+      en: 'Empire Partners is a new affiliate program, so all our offers are in-house',
+      ru: 'Empire Partners является новой партнерской программой, поэтому все наши офферы in-house',
+    },
     question2: {
       en: 'Traffic from which countries do&nbsp;you accept?',
       ru: 'Трафик из&nbsp;каких стран вы&nbsp;принимаете?',
+    },
+    answer2: {
+      en: `At the moment Empire Partners affiliate program works with the following countries: IN, BD, PK, TR, CA
+      The list of countries is constantly changing and expanding, so it is better to check with your affiliate manager for up-to-date information.`,
+      ru: `На данный момент партнерская программа Empire Partners работает со следующими странами: IN, BD, PK, TR, CA
+      Список стран постоянно изменяется и расширяется, поэтому лучше уточнять актуальную информацию у Вашего аффилиейт менеджера.`,
     },
     question3: {
       en: 'How much do&nbsp;you pay for engaged user?',
       ru: 'Сколько вы&nbsp;платите за&nbsp;одного привлеченного юзера?',
     },
+    answer3: {
+      en: `We work with CPA/RevShare/Hybrid work models.<br>
+      The rate depends on the GEO and the source of your traffic.<br>
+      All conditions can be discussed with your personal affiliate manager.`,
+      ru: `Работаем по разным моделям работы CPA/RevShare/Hybrid.<br>
+      Ставка зависит от ГЕО и источника вашего траффика.<br>
+      Все условия можно обсудить с персональным аффилиэйт менеджером.`,
+    },
     question4: {
       en: 'What are the restrictions on&nbsp;the sources of&nbsp;traffic?',
       ru: 'Какие ограничения по&nbsp;источникам трафика?',
+    },
+    answer4: {
+      en: `The affiliate program specializes at attracting reliable traffic sources,
+      excluding fraud and motivated players. We do not accept email spam, social media
+      spam or any other form of spam. Any manipulation of cookies is prohibited.`,
+      ru: `Партнерская программа специализируется на привлечении надежных источников
+      трафика, исключая фрод и мотивированных игроков. Мы не принимаем почтовый спам,
+      спам в социальных сетях и любой другой вид спама. Запрещены любые манипуляции с cookies.`,
     },
     question5: {
       en: 'What payment methods do&nbsp;you support?',
       ru: 'Какие платежные методы вы&nbsp;поддерживаете?',
     },
+    answer5: {
+      en: 'TRON (trc20)',
+      ru: 'TRON (trc20)',
+    },
     question6: {
-      en: 'What is&nbsp;the minimum payment?',
+      en: 'What is the minimum withdrawal amount?',
       ru: 'Какая минимальная сумма вывода?',
     },
+    answer6: {
+      en: 'The minimum withdrawal amount from an affiliate account is $100.',
+      ru: 'Минимальная сумма вывода денег с аффилиэйт счета - 100$.',
+    },
     question7: {
-      en: 'Does your affiliate programe offer creatives?',
-      ru: 'Предоставляет&nbsp;ли ваша аффилиейт программа креативы?',
+      en: 'Does your affiliate program provide creatives?',
+      ru: 'Предоставляет ли ваша аффилиейт программа креативы?',
+    },
+    answer7: {
+      en: `Yes, our managers are ready to provide converting and relevant landing pages and banners for
+      your advertising campaigns, or you can find them in your personal account.`,
+      ru: `Да, наши менеджеры готовы предоставить конвертящие и актуальные лендинги и баннеры для ваших
+      рекламных кампаний, либо вы можете их увидеть в своем личном кабинете.`,
     },
     question8: {
-      en: 'Where can&nbsp;I get promo materials?',
-      ru: 'Где я&nbsp;могу получить баннеры и&nbsp;другие промо-материалы?',
+      en: 'How often do you make payments?',
+      ru: 'Как часто вы проводите выплаты?',
+    },
+    answer8: {
+      en: 'At the moment we pay partners twice a month.',
+      ru: 'На данный момент мы платим партнерам два раза в месяц',
     },
   };
 
   const langParent = document.querySelector('.js_lang');
   const langHead = langParent.querySelector('.js_lang-head');
   const langHeadVal = langHead.querySelector('.js_lang-val');
-  const langItems = langParent.querySelectorAll('li');
+  const langBtns = langParent.querySelectorAll('[data-btn]');
 
   langHead.addEventListener('click', () => {
     langParent.classList.toggle('open');
   });
 
   function changeLang() {
+    langBtns.forEach((btn) => {
+      btn.style.display = 'block';
+      if (btn.dataset.btn === currentLang) {
+        btn.style.display = 'none';
+        langHeadVal.textContent = btn.textContent;
+      }
+    });
+
     for (const key in textHomePage) {
       const elements = document.querySelectorAll(`[data-lang=${key}]`);
       elements.forEach((element) => {
@@ -157,14 +209,10 @@ const lang = () => {
     }
   }
 
-  langItems.forEach((langItem) => {
-    langItem.addEventListener('click', () => {
-      const button = langItem.querySelector('[data-btn]');
-      let langText = button.textContent;
-      currentLang = button.getAttribute('data-btn');
-      button.setAttribute('data-btn', currentLang === 'en' ? 'ru' : 'en');
-      button.textContent = langHeadVal.textContent;
-      langHeadVal.textContent = langText;
+  langBtns.forEach((langBtn) => {
+    langBtn.addEventListener('click', () => {
+      currentLang = langBtn.dataset.btn;
+      localStorage.setItem('language', langBtn.dataset.btn);
       langParent.classList.remove('open');
       changeLang();
     });
