5d3d48b7bc8720b00c54e5cfdde511ae28fde22d
Oleg <mr.rooby@mail.ru>
2024-02-01 23:05:52 +0300

    Fix bugs

--- betcity/brandings/soccer/js/main.js
+++ betcity/brandings/soccer/js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	document.body.classList.add(getOS());
 	document.body.classList.add('loaded');
 
@@ -40,6 +40,36 @@ function getOS() {
 
 	return os;
 }
+
+// create element
+const scrollElement = document.createElement('div');
+
+// add styles
+scrollElement.style.width = '100px';
+scrollElement.style.height = '100px';
+scrollElement.style.overflow = 'scroll';
+scrollElement.style.visibility = 'hidden';
+scrollElement.style.position = 'absolute';
+scrollElement.style.top = '-9999px';
+
+document.body.appendChild(scrollElement);
+
+const scrollbarWidth = scrollElement.offsetWidth - scrollElement.clientWidth;
+
+document.body.removeChild(scrollElement);
+
+// check window width and add class
+function trackWidthChanges() {
+	const bodyWrap = document.querySelector('.body-wrap');
+	if (bodyWrap.clientWidth + scrollbarWidth <= 1626) {
+		bodyWrap.classList.add('mq');
+	} else {
+		if (bodyWrap.classList.contains('mq')) bodyWrap.classList.remove('mq');
+	}
+}
+
+window.addEventListener('load', trackWidthChanges);
+window.addEventListener('resize', trackWidthChanges);
 
 function animateWowCoin() {
 	const wow = document.querySelectorAll('.header__abanner__wow');
